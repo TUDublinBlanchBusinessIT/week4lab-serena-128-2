bc2064a8a80b9f62dbb4a6aaa6447747bc36a292
B00141403 Serena Emedeke <b00141403@mytudublin.ie>
2024-02-12 17:07:04 +0000

    part 3

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-	Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
-	Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
+
+
+});
 	
 	Route::get('/customers/edit/{id}', 'App\Http\Controllers\CustomerController@edit');
 	Route::post('/customers/update', 'App\Http\Controllers\CustomerController@update');
-	
-});
+	Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+	Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
+	Route::get('/customers/edit/{id}', 'App\Http\Controllers\CustomerController@edit');
+	Route::post('/customers/update', 'App\Http\Controllers\CustomerController@update');
